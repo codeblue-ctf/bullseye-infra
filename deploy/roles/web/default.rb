622411d8cb768b948ec4b1390be9e3fed42da156
icchy <icchyr@gmail.com>
2018-10-14 00:21:05 +0900

    kasu2

--- deploy/roles/web/default.rb
+++ deploy/roles/web/default.rb
@@ -5,3 +5,27 @@ package "ruby-dev"
 package "gcc"
 package "g++"
 package "libsqlite3-dev"
+
+execute "bundle install" do
+  user "#{node[:user]}"
+  cwd "#{node[:app_path]}"
+  command "bundle install --path=vendor/bundle"
+end
+
+%w(
+  db:migrate
+  db:seed
+  assets:precompile
+).each do |task|
+  execute "rails #{task}" do
+    user "#{node[:user]}"
+    cwd "#{node[:app_path]}"
+    command "bundle exec rails #{task}"
+  end
+end
+
+# execute "rails server" do
+#   user "#{node[:user]}"
+#   cwd "#{node[:app_path]}"
+#   command "bundle exec rails s"
+# end

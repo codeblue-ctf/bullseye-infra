6be7e741c234cd6ebef4f5d4ab31fc4b816faea5
tyage <namatyage@gmail.com>
2018-10-21 19:58:36 +0900

    start nginx

--- deploy/roles/web/default.rb
+++ deploy/roles/web/default.rb
@@ -1,10 +1,9 @@
 include_recipe "../../cookbooks/ruby"
 include_recipe "../../cookbooks/redis"
 
-package "ruby-dev"
-package "gcc"
-package "g++"
-package "libsqlite3-dev"
+%w(ruby-dev gcc g++ libsqlite3-dev).each do |pkg|
+  package pkg
+end
 
 execute "bundle install" do
   user "#{node[:user]}"
@@ -24,12 +23,13 @@ end
   end
 end
 
+# register bullseye web service
 execute 'systemctl daemon-reload' do
   action :nothing
 end
 
 template '/etc/systemd/system/bullseye-web.service' do
-  source 'templates/bullseye-web.service'
+  source 'templates/systemd/bullseye-web.service'
   owner 'root'
   group 'root'
   mode  '0644'
@@ -38,4 +38,22 @@ end
 
 service 'bullseye-web' do
   action [:enable, :start]
+end
+
+# start nginx
+
+package 'nginx'
+service 'nginx' do
+  action [:enable, :start]
+end
+file "/etc/nginx/sites-enabled/default" do
+  action :delete
+  notifies :reload, 'service[nginx]'
+end
+template '/etc/nginx/sites-enabled/bullseye-web' do
+  source 'templates/nginx/bullseye-web'
+  owner  'root'
+  group  'root'
+  mode   '644'
+  notifies :reload, 'service[nginx]'
 end

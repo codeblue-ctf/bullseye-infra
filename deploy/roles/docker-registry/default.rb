b0570b84710fc172ccc7389cf7c295c0fa5bc895
tyage <namatyage@gmail.com>
2018-10-27 18:47:53 +0900

    set docker plugin config

--- deploy/roles/docker-registry/default.rb
+++ deploy/roles/docker-registry/default.rb
@@ -1,3 +1,9 @@
+node.reverse_merge!(
+  docker: {
+    users: [ node[:user] ]
+  }
+)
+
 include_recipe "../../cookbooks/deploy_key"
 include_recipe "../../cookbooks/known_hosts"
 include_recipe "docker::install"

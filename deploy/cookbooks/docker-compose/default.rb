57b2826df1d57c3c687f6f311cb5ef7b582809ae
tyage <namatyage@gmail.com>
2018-10-27 18:27:29 +0900

    download docker-compose file if not exists

--- deploy/cookbooks/docker-compose/default.rb
+++ deploy/cookbooks/docker-compose/default.rb
@@ -1,7 +1,10 @@
 execute "download docker-compose" do
-  command 'sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+  command 'curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+  user 'root'
+  not_if "test -e /usr/local/bin/docker-compose"
 end
 
 execute "install docker-compose" do
-  command "sudo chmod +x /usr/local/bin/docker-compose"
+  command 'chmod +x /usr/local/bin/docker-compose'
+  user 'root'
 end

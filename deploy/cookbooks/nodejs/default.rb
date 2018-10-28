30ac82f5959ab136f58dcd8b15aecc1d636b9db5
tyage <namatyage@gmail.com>
2018-10-28 16:04:26 +0900

    start bullseye runner service

--- deploy/cookbooks/nodejs/default.rb
+++ deploy/cookbooks/nodejs/default.rb
@@ -1,3 +1,4 @@
 include_recipe "../apt_update"
 
 package "nodejs"
+package "npm"
--- deploy/roles/runner/default.rb
+++ deploy/roles/runner/default.rb
@@ -9,3 +9,22 @@ include_recipe "../../cookbooks/known_hosts"
 include_recipe "docker::install"
 include_recipe "../../cookbooks/docker-compose"
 include_recipe "../../cookbooks/nodejs"
+
+execute "npm install" do
+  user node[:user]
+  cwd node[:app_path]
+  command "npm install"
+end
+
+# Register bullseye runner service and start
+template '/etc/systemd/system/bullseye-runner.service' do
+  source 'templates/systemd/bullseye-runner.service'
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+end
+
+service 'bullseye-runner' do
+  action [:enable, :start]
+end
